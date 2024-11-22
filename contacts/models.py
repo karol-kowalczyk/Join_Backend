cf2f206da812d72eb0f5ca035530bd52a763ce49
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-11-22 01:34:17 +0100

    implemented a new class, where i can define a task

--- contacts/models.py
+++ contacts/models.py
@@ -7,3 +7,22 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.email})"
+    
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=50)
+    due_date = models.DateField()
+    
+    PRIORITY_CHOICES = [
+        ('urgent', 'Urgent'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    ]
+    
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='low',
+    )
+
+    subtasks = models.CharField(max_length=50)
